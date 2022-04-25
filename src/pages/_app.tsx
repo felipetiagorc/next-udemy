fc5348bca4928317224911d63d4cf2ad69245a75
Felipe <felipetiagorc@yahoo.com.br>
2022-04-25 17:37:52 -0300

    tailwind ok

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,39 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { createGlobalStyle } from "styled-components"
 // import Layout from '../components/layout'
 import './../styles/global.css'
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margins: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #__next{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  height: 100%;
+}
+`
+
+const temaPadrao = {
+  colors: {
+    primary: '#0070f3'
+  }
+}
+
+
 
 function App({ Component, pageProps }: AppProps) {
   return (
     // Layout 'persist' across all pages
     // <Layout>
     <>
+    <GlobalStyle />
+    
       <Head>
         <title>GPU-Next</title>
         <link rel="shortcut icon" href="/img/favicon-16x16.png" />
@@ -16,7 +42,7 @@ function App({ Component, pageProps }: AppProps) {
 
         <meta name="theme-color" content="#06092b" />
       </Head>
-      {/* <GlobalStyles /> */}
+
       <Component {...pageProps} />
     </>
     // </Layout>
