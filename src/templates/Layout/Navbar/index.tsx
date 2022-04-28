6ff1445490bb39879d047e524e63cfa9e89f268e
Felipe <felipetiagorc@yahoo.com.br>
2022-04-28 18:00:04 -0300

    chakra ok

--- src/templates/Layout/Navbar/index.tsx
+++ src/templates/Layout/Navbar/index.tsx
@@ -18,11 +18,13 @@ export default function NavBar() {
       h="95vh"
       marginTop="2.5vh"
       boxShadow="0 4px 12px 0 rgba(0,0,0,0.05)"
-      w="200px"
+      borderRadius={navSize == "small" ? "15px" : "30px"}
+      w={navSize == 'small' ? "75px" : "200px"}
       flexDir="column"
       justifyContent="space-between"
+      backgroundColor="red.200"
     >
-      <Flex p="5%" flexDir="column" alignItems="flex-start" as="nav">
+      <Flex p="5%" flexDir="column" alignItems="flex-end" as="nav">
         <IconButton
           background="none"
           mt={5}
@@ -35,11 +37,11 @@ export default function NavBar() {
           }}
         />
       </Flex>
-      <Flex p="5%" flexDir="column" w="100%" alignItems="flex-start" mb="4">
-        <Divider />
+      <Flex p="5%" flexDir="column" w="100%" alignItems={navSize == "small" ? "center": "flex-start"} mb={4}>
+        <Divider display={navSize == "small" ? "none": "flex"} />
         <Flex mt="4" align="center">
           <Avatar size="sm" src="" />
-          <Flex flexDirection="column" ml="4">
+          <Flex flexDirection="column" ml="4" display={navSize == "small" ? "none": "flex"}>
             <Heading as="h3" size="sm">
               Silvio
             </Heading>
